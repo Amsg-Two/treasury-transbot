b0a4e1adc3a670c7a776251d8ade49e4689c8d4b
Sunskimmer82 <92886026+Sunskimmer822@users.noreply.github.com>
2022-11-30 13:48:37 -0600

    add the actual bot

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const Discord = require('discord.js');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 //just the quick required djs class
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 //client instancing n shit
+const { config } = require('./config.json');
+
+//log when bot is ready, show which user the bot is logged in as in the Console
+client.once('Ready', () => {
+  console.log("Ready to trans the brits, logged in as "+client.user.username);
+})
+
+client.on('messageCreate', msg => {
+if (msg.guildid == '1029033907112390756') {
+  msg.react('🏳️‍⚧️').then(console.log).catch(console.error);
+  console.log('Reacted to '+msg.author+' with the trans flag at '+new Date());
+}
+})
++client.login(token);
