3f9890059442b042e19fa13a8a96add970efab5a
D_mon <82187465+Amsg-Two@users.noreply.github.com>
2022-11-30 13:35:14 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+//just the quick required djs class
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//client instancing n shit
